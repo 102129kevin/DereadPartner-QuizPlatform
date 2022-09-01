e6d52129e2be49dbb45faa7603812046bc588e73
102129kevin <102129kevin@gmail.com>
2022-09-01 17:57:34 +0800

    test0901

---
+++ server.js
@@ -0,0 +1,15 @@
+// Load Node modules
+var express = require('express');
+var path = require('path');
+// Initialise Express
+var app = express();
+
+// Render static files
+app.use('/examples', express.static(path.join(__dirname, 'examples')));
+app.use('/dist', express.static(path.join(__dirname, 'dist')));
+//app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// Port website will run on
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
