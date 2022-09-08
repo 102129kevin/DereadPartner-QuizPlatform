e9d8063607c021c2498becf878e0f575d0a5d49c
102129kevin <102129kevin@gmail.com>
2022-09-08 19:01:48 +0800

    'testdelete'

--- public/js/nav.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", function (event) {
-  const mobileMenu = document.querySelector(".mobile-menu");
-  const navbar = document.querySelector(".navbar");
-  mobileMenu.addEventListener("click", function () {
-    navbar.classList.toggle("open");
-  });
-});
